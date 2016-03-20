38859494684e9a950b8e040da46b9aa814cbad25
Mithun Manivannan <mithunm93@gmail.com>
2016-03-20 00:54:04 -0700

    Trying CasperJS
    Hit a roadblock with continuously getting 500's when using node, not sure
    what the issue is but I decided I'll try using a headless browser instead.
    Seamless doesn't do API keys anymore so I'll have to resort to virtually
    clicking through the website.
    
    The app will have to be run in two parts: 1 being the node.js server that
    receives the order from Merakians and updates the Firebase entry accordingly,
    and one is this casperJS bot that can retrieve data from Firebase and make
    the orders accordingly.

---
+++ sample.js
@@ -0,0 +1,39 @@
+var casper = require('casper').create({
+  verbose: true,
+  logLevel: 'debug',
+  pageSettings: {
+    loadImages:  false,         // The WebPage instance used by Casper will
+    loadPlugins: false,         // use these settings
+    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
+  }
+});
+var private = require('./private');
+
+casper.on('remote.message', function(msg) {
+    this.echo('remote message caught: ' + msg);
+});
+
+casper.on("page.error", function(msg, trace) {
+    this.echo("Page Error: " + msg, "ERROR");
+});
+
+var url = 'https://www.seamless.com/corporate/login';
+
+casper.start(url);
+
+// 1. Login
+casper.waitForSelector('form#widgetLoginForm', function() {
+  console.log("main page loaded");
+  this.fill('form#widgetLoginForm', {
+    'username': private.username,
+    'password':  private.password
+  }, true);
+});
+
+// 2. Select 5:45 delivery
+casper.waitForSelector('form#pageForm', function() {
+  console.log("Step 1: Time selection loaded");
+  this.fill('form#pageForm', {'time': '5:45 PM'}, true);
+});
+
+casper.run();
